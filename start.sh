92e9a412bf92b2734d236f2c5beaebfd4486dd4f
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2022-12-02 17:38:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
+git clone https://github.com/arthur-dane/Shizukabot /Mdisk-Search-Bot
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
+echo "Starting Bot....@movies_halt"
 python3 main.py
